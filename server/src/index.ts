16ae5d9ec183a638d168056cec0f913ccc2546ba
bifinbabu <bifinbabu088@gmail.com>
2025-01-01 00:50:00 +0530

    Server initial

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+// import connectDB from "./utils/dbConnect";
+// import taskRoutes from "./routes/taskRoutes";
+
+dotenv.config();
+// connectDB();
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "TODO api server" });
+});
+// app.use("/api", taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
