48db502b97b8348288a952d91f39c58a08555260
bifinbabu <bifinbabu088@gmail.com>
2025-01-03 17:42:45 +0530

    Fix: Swagger UI custom css

--- server/src/routes/apiDocs.ts
+++ server/src/routes/apiDocs.ts
@@ -8,6 +8,8 @@ const router = express.Router();
 
 const openApiPath = path.join(__dirname, "../openapi.yaml");
 const openApiSpec = YAML.load(openApiPath);
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 // Serve Swagger UI for testing
 router.use("/api-test", swaggerUi.serve);
@@ -20,8 +22,9 @@ router.get(
       displayRequestDuration: true,
       filter: true,
     },
-    customCss: ".swagger-ui .topbar { display: none }",
-    customSiteTitle: "To-Do API Testing",
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl: CSS_URL,
   })
 );
 
